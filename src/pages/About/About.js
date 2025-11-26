796959e44deaab9c1aebd572049bb0c91d79ec18
Godwin Chinkwe <chinkwegodwin@gmail.com>
2025-11-26 11:41:32 +0100

    seconf

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { motion } from 'framer-motion';
 import { FaAward, FaUsers, FaHeart, FaHandshake } from 'react-icons/fa';
 import './About.css';
